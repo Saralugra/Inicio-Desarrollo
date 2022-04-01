88b8d4935615907db48a1163b8d23ac5b4863cda
saralugra <saralu1885.slg@gmail.com>
2022-03-31 20:31:10 -0500

    delete words and lines

--- src/project.clj
+++ src/project.clj
@@ -1,5 +1,3 @@
 (ns project)
 (def filereader (slurp "resources/Lista Consolidada del Consejo de Seguridad de las Naciones Unidas.txt"))
 (def filereaderOFAC (slurp "resources/LISTA OFAC.txt"))
-filereader
-filereaderOF
